b44d9fe78fd2c4fd442f282bd8b3cb65563679bf
Srishti Pandey <77576448+Srishti-Pandey10@users.noreply.github.com>
2021-10-30 15:57:28 +0530

    Archives page 1 and ArchivesPage2 by Srishti

--- src/App.js
+++ src/App.js
@@ -3,16 +3,21 @@ import Loader from './components/loader';
 import Main from './components/main.js'
 import Navbar from './components/navbar';
 import Event from './components/eventsPopup'
-import Archives from './components/archives'
+import ArchivesPage1 from './components/ArchivesPage1'
+
 import Projects from './components/projects'
 import ParticleEffect from './components/particleeffect';
 import Scene from './components/threejs';
 import MainThree from './components/mainThree/mainthree';
+import ArchivesPage2 from './components/ArchivesPage2';
 
 function App() {
   return (
     <>
       <Navbar />
+      
+      <ArchivesPage1 />
+      <ArchivesPage2 />
       <MainThree />
       {/* <Main />
       <Loader />
