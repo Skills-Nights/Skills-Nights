89d6e64ee5502e30f9062985b7060d7b2733b4fe
ManasKapoor18 <manasmk2016@gmail.com>
2021-10-30 22:49:59 +0530

    our team section completed

---
+++ src/components/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+import Team from './components/Team';
+
+function App() {
+  return (
+    <div className="App">
+       <Team /> 
+    </div>
+     
+  );
+}
+
+export default App;
