8ab508d41eb376a05762e5e459f0ff98d96616ce
Bhavesh <bhic2030@gmail.com>
2021-12-16 17:31:24 +0530

    adding navbar

--- client/src/components/navbar/Navbar.js
+++ client/src/components/navbar/Navbar.js
@@ -1,32 +1,72 @@
-import React from 'react'
+import React, {useState} from 'react'
 import "./Navbar.css"
 import logo from "./assets/logo.jpg"
 import {HashLink} from "react-router-hash-link"
 
 const Navbar = () => {
+
+    const [show, setShow] = useState(false);
+
     return (
       <div className="navbar-container">
         <nav className="navbar">
           <div className="nav-logo">
             <img src={logo} alt="sn-logo" />
           </div>
-          <div className="nav-links">
+          <div
+            onClick={() => setShow((prevState) => !prevState)}
+            className={`${show ? "hamburger-btn open" : "hamburger-btn"}`}
+          >
+            <div className="hamburger"></div>
+          </div>
+          <div className={`${show ? "nav-links" : "nav-links hide"}`}>
             <ul>
-              <HashLink smooth to="/#about-us">
-                <li>ABOUT US</li>
-              </HashLink>
-              <HashLink smooth to="/#events" >
-                <li>EVENTS</li>
-              </HashLink>
-              <HashLink smooth to="/#archives" >
-                <li>ARCHIVES</li>
-              </HashLink>
-              <HashLink smooth to="/#our-team" >
-                <li>OUR TEAM</li>
-              </HashLink>
-              <HashLink smooth to="/#connect-with-us" >
-                <li>CONNECT WITH US</li>
-              </HashLink>
+              <li className="menu-title">Menu</li>
+              <li>
+                <HashLink
+                  onClick={() => setShow((prevState) => !prevState)}
+                  smooth
+                  to="/#about-us"
+                >
+                  ABOUT US
+                </HashLink>
+              </li>
+              <li>
+                <HashLink
+                  onClick={() => setShow((prevState) => !prevState)}
+                  smooth
+                  to="/#events"
+                >
+                  EVENTS
+                </HashLink>
+              </li>
+              <li>
+                <HashLink
+                  onClick={() => setShow((prevState) => !prevState)}
+                  smooth
+                  to="/#archives"
+                >
+                  ARCHIVES
+                </HashLink>
+              </li>
+              <li>
+                <HashLink
+                  onClick={() => setShow((prevState) => !prevState)}
+                  smooth
+                  to="/#our-team"
+                >
+                  OUR TEAM
+                </HashLink>
+              </li>
+              <li>
+                <HashLink
+                  onClick={() => setShow((prevState) => !prevState)}
+                  smooth
+                  to="/#connect-with-us"
+                >
+                  CONNECT WITH US
+                </HashLink>
+              </li>
             </ul>
           </div>
         </nav>
